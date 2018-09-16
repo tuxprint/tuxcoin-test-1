b749d21cbe23c17c6eb19c12dab9280a8084d605
tuxprint <dev@tuxcoin.io>
2018-09-15 20:09:25 -0500

    First commit

---
+++ src/crypto/allium/alliumV2.h
@@ -0,0 +1,14 @@
+#ifndef ALLIUMV2_H
+#define ALLIUMV2_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void alliumV2_hash(const char* input, char* output);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
